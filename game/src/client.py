a7f74b39bc4726578495ca44506f6f35fa2518d9
sgl <329162349@qq.com>
2022-08-17 15:36:20 +0800

    finished client

--- game/src/client.py
+++ game/src/client.py
@@ -6,8 +6,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+# 用来从终端读取信息的包
+from sys import stdin
 
-def main():
+def operate(op, user_id, user_name, score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -23,11 +25,23 @@ def main():
     # Connect!
     transport.open()
 
-    user = User(1, 'sgl', 1500)
-    client.add_user(user, "")
+    #
+    user = User(user_id, user_name, score)
+
+    # 对操作进行判断添加用户还是删除用户
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
 
     # Close!
     transport.close()
+
+def main():
+    # 读入信息
+    for line in stdin:
+        op, user_id, user_name, score = line.split(' ')
+        operate(op, int(user_id), user_name, int(score))
 
 
 if __name__ == "__main__":
